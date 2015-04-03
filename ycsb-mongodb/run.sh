db32fc852a46e0036fb11620b65f561da5c55b02
Rui Zhang <rui.zhang@mongodb.com>
2015-04-03 13:47:47 +0000

    capture stderr as well

--- ycsb-mongodb/run.sh
+++ ycsb-mongodb/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/bin/ycsb load mongodb -s -P workloads/workloadShard -p mongodb.url=10.2.1.99:27017 -threads 64 | tee loadPhase.log
-/bin/ycsb run  mongodb -s -P workloads/workloadShard -p mongodb.url=10.2.1.99:27017 -threads 64 | tee  runPhase.log
+./bin/ycsb load mongodb -s -P workloads/workloadShard -p mongodb.url=10.2.1.99:27017 -threads 64 2>&1 | tee loadPhase.log
+./bin/ycsb run  mongodb -s -P workloads/workloadShard -p mongodb.url=10.2.1.99:27017 -threads 64 2>&1 | tee  runPhase.log
