1d37a51880e8a687474037965d3629aaa0f11cc6
Rui Zhang <rui.zhang@mongodb.com>
2015-06-30 11:31:27 -0700

    fix run.sh

--- ycsb-mongodb/run.sh
+++ ycsb-mongodb/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVER="10.2.1.100"
+SERVER="10.2.0.100"
 
 ./bin/ycsb load mongodb -s -P workloads/workloadShard -p mongodb.url=$SERVER:27017 -threads 64 2>&1 | tee loadPhase.log
 ./bin/ycsb run  mongodb -s -P workloads/workloadShard -p mongodb.url=$SERVER:27017 -threads 64 2>&1 | tee  runPhase.log
