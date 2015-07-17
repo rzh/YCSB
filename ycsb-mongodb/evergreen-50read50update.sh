7903610f356eff0fe3434643c4a620e32356e6e6
Rui Zhang <rui.zhang@mongodb.com>
2015-07-17 13:54:23 -0700

    change evergreen 5050 to 64 thread

--- ycsb-mongodb/evergreen-50read50update.sh
+++ ycsb-mongodb/evergreen-50read50update.sh
@@ -2,4 +2,4 @@
 
 SERVER=$1
 
-./bin/ycsb run  mongodb -s -P workloads/workloadEvergreen_50read50update -p mongodb.url=$SERVER:27017 -threads 32 
+./bin/ycsb run  mongodb -s -P workloads/workloadEvergreen_50read50update -p mongodb.url=$SERVER:27017 -threads 64 
