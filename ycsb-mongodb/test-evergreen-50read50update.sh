2ea1b0a22e7cd2aa9301c54e4582502157d69880
Rui Zhang <rui.zhang@mongodb.com>
2015-08-28 13:42:47 -0700

    Checkin test run script

--- ycsb-mongodb/test-evergreen-50read50update.sh
+++ ycsb-mongodb/test-evergreen-50read50update.sh
@@ -2,4 +2,4 @@
 
 SERVER="10.2.0.100"
 
-./bin/ycsb run  mongodb -s -P workloads/workloadEvergreen_50read50update -p mongodb.url=$SERVER:27017 -threads 32 
+./bin/ycsb run  mongodb -s -P workloads/test_workloadEvergreen_50read50update -p mongodb.url=$SERVER:27017 -threads 32 
